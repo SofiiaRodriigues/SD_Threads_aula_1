cda0ca7b618cdee179cd6775355714406cf9599a
Luís Lobo <ellobo@ipb.pt>
2021-03-14 16:32:51 +0000

    - add solutions

---
+++ ds-threads/src/main/java/pt/ipb/dsys/threads/deadlock/account/DeadlockTransferMain.java
@@ -0,0 +1,12 @@
+package pt.ipb.dsys.threads.deadlock.account;
+
+public class DeadlockTransferMain {
+
+    public static void main(String[] args) {
+        Account myAccount = new Account(1000, 10);
+        Account yourAccount = new Account(2000, 20);
+        new Thread(() -> Account.transferSolved(myAccount, yourAccount, 5)).start();
+        new Thread(() -> Account.transferSolved(yourAccount, myAccount, 3)).start();
+    }
+
+}
