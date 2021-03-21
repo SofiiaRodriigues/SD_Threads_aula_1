7bc45df17c6c7bb1a258628ad7afedb56b6589b9
Luís Lobo <ellobo@ipb.pt>
2021-03-21 23:25:39 +0000

    - english please - remove unecessary try-with-resources

--- ds-communication/src/main/java/pt/ipb/dsys/com/intro/SocketClientMain.java
+++ ds-communication/src/main/java/pt/ipb/dsys/com/intro/SocketClientMain.java
@@ -2,7 +2,6 @@ package pt.ipb.dsys.com.intro;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -10,11 +9,9 @@ public class SocketClientMain {
 
     public static void main(String[] args) {
         try (Socket echoSocket = new Socket(SocketServerMain.HOSTNAME, SocketServerMain.PORT)) {
-            try (OutputStream os = echoSocket.getOutputStream()) {
-                try (PrintWriter writer = new PrintWriter(os)) {
-                    String message = JOptionPane.showInputDialog(null, "Mensagem para o servidor?", "CLIENTE", JOptionPane.QUESTION_MESSAGE);
-                    writer.println(message);
-                }
+            try (PrintWriter output = new PrintWriter(echoSocket.getOutputStream())) {
+                String message = JOptionPane.showInputDialog(null, "Message to server?", "CLIENT", JOptionPane.QUESTION_MESSAGE);
+                output.println(message);
             }
         } catch (IOException ex) {
             System.out.printf("IO Error: %s\n", ex.getMessage());
