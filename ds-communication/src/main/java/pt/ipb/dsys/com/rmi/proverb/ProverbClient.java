3367af23fabba01a65df4036a975044d589e9e19
Luís Lobo <ellobo@ipb.pt>
2021-04-05 20:55:29 +0100

    - fix client - add run conf

--- ds-communication/src/main/java/pt/ipb/dsys/com/rmi/proverb/ProverbClient.java
+++ ds-communication/src/main/java/pt/ipb/dsys/com/rmi/proverb/ProverbClient.java
@@ -1,5 +1,6 @@
 package pt.ipb.dsys.com.rmi.proverb;
 
+import javax.swing.*;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -10,9 +11,16 @@ public class ProverbClient {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("127.0.0.1");
-            RemoteProverb stub = (RemoteProverb) registry.lookup(ProverbProvider.PROTOCOL_NAME);
-            String output = stub.processInput(null);
-            System.out.println(output);
+            RemoteProverb remote = (RemoteProverb) registry.lookup(ProverbProvider.PROTOCOL_NAME);
+
+            String fromServer;
+            String fromUser = "";
+
+            while ((fromServer = remote.processInput(fromUser)) != null) {
+                if (fromServer.equals("Bye."))
+                    break;
+                fromUser = JOptionPane.showInputDialog(fromServer);
+            }
         } catch (RemoteException | NotBoundException e) {
             System.out.printf("Erro: %s\n", e.getMessage());
         }
